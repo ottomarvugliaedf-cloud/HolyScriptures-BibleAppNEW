63c78abc25601dfecc53c187fc0760047389aa05
ottomarvugliaedf-cloud <otto.marvuglia.edf@gmail.com>
2026-02-11 21:50:48 +0100

    Initialize SoLoader in MainApplication
    Initialized SoLoader in onCreate method to fix registerCallableModule.

--- android/app/src/main/java/com/holyscriptures/MainApplication.kt
+++ android/app/src/main/java/com/holyscriptures/MainApplication.kt
@@ -12,6 +12,7 @@ import com.facebook.react.ReactHost
 import com.facebook.react.common.ReleaseLevel
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
 import com.facebook.react.defaults.DefaultReactNativeHost
+import com.facebook.soloader.SoLoader
 
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
@@ -40,6 +41,7 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
+    SoLoader.init(this, false)  // <-- AGGIUNTO per fix registerCallableModule
     DefaultNewArchitectureEntryPoint.releaseLevel = try {
       ReleaseLevel.valueOf(BuildConfig.REACT_NATIVE_RELEASE_LEVEL.uppercase())
     } catch (e: IllegalArgumentException) {
